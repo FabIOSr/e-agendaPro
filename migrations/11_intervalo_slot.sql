a3b0e4dd40412e52a8dabed479c09ee391391040
Fábio Ramos <fabio-s-ramos@hotmail.com>
2026-03-27 17:44:52 -0300

    fix: adiciona DEFAULT 0 na coluna intervalo_slot

--- migrations/11_intervalo_slot.sql
+++ migrations/11_intervalo_slot.sql
@@ -1,4 +1,4 @@
 -- 11_intervalo_slot.sql
 -- Adiciona campo para configurar intervalo entre slots (plano pago)
 
-ALTER TABLE prestadores ADD COLUMN IF NOT EXISTS intervalo_slot int4;
+ALTER TABLE prestadores ADD COLUMN IF NOT EXISTS intervalo_slot int4 DEFAULT 0;
